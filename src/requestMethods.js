84b59f321585e8283ff64e28718651604adaaf9b
gerardonastri <gerardonastri06@gmail.com>
2022-06-15 12:50:47 +0200

    better

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const  BASE_URL = "http://localhost:8000/api/";
+const  BASE_URL = "https://django-react-ecommerce-api.herokuapp.com/api/";
 
 export const publicRequest = axios.create({
     baseURL:  BASE_URL
